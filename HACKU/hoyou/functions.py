0ef0bedbb65b5d70b8df7ad8b44bd993073452b4
fusawayugo <fusawayugo0712@g.ecc.u-tokyo.ac.jp>
2023-09-14 17:50:20 +0900

    内積計算、record作成用の関数をhoyou/functions.pyに作成しました

--- HACKU/hoyou/functions.py
+++ HACKU/hoyou/functions.py
@@ -1,7 +1,30 @@
 from datetime import datetime
+from .models import Person,Record
+import numpy as np
+import pickle
 
 def get_datetime(date_str,time_str):
     date_obj = datetime.strptime(date_str, '%Y-%m-%d') 
     time_obj = datetime.strptime(time_str, '%H:%M') 
     combined_datetime = datetime.combine(date_obj.date(), time_obj.time())
     return combined_datetime
+
+#内積計算
+def inner_product(vector):
+    persons=Person.objects.all()
+    max=-float('inf')
+    id=None
+    #登録してない人が通った場合はまた考えしょう
+    for person in persons:
+        person_vector=pickle.loads(person.vector)
+        m=np.dot(person_vector,vector)
+        if m>max:
+            max=m
+            id=person.id
+    return id
+
+
+#person.idと出退勤のデータからRecordobjectの作成
+def create_record(person_id,shuttai_data):
+    person=Person.objects.get(id=person_id)
+    Record.objects.create(person=person,shuttai=shuttai_data)

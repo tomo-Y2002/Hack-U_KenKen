76f22a1039c830cd3415063b7f80d231a99f106a
Tomohiro Yamashita <tomohiro2014yamashita@gmail.com>
2023-09-16 08:29:22 +0900

    bounding boxに名前を付けられるようになったよ

--- video2vec/streaming.py
+++ video2vec/streaming.py
@@ -17,12 +17,28 @@ while cap.isOpened():
         # Run YOLOv8 inference on the frame
         results_gen = model(frame, 
                             classes=0,
-                            conf=0.7,
+                            conf=0.5,
                             stream = True)
         
         for result in results_gen:  # Iterate over generator results
             # Visualize the result on the frame
             annotated_frame = result.plot()
+            
+            if len(result.boxes.xyxy) >= 1:
+                x1 = int(result.boxes.xyxy[0][0])   #左上
+                y1 = int(result.boxes.xyxy[0][1])   #左上
+                x2 = int(result.boxes.xyxy[0][2])   #右下
+                y2 = int(result.boxes.xyxy[0][3])   #右下
+            
+                # 名前を記述できるかを試す。
+                cv2.putText(annotated_frame,
+                            text='yamashita',
+                            org=(x2-100, y1),
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale=1.0,
+                            color=(123, 0,122),
+                            thickness=2,
+                            lineType=cv2.LINE_4)
 
             # Display the annotated frame
             cv2.imshow("YOLOv8 Inference", annotated_frame)
